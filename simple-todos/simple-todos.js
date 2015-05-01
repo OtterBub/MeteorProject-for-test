928877bb162ce028ae4f555422fc4adaef9322aa
OtterBub <gamepsk@hanmail.net>
2015-05-01 17:58:44 +0900

    Only Simple-Todos Commit

---
+++ simple-todos/simple-todos.js
@@ -0,0 +1,34 @@
+
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+    
+    
+  });
+  
+  Template.body.helpers({
+    tasks: function() {
+      return Tasks.find();
+    }
+    });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
