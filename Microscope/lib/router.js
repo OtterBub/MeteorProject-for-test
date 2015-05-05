15f821dd8b9fd4c7b3d504927533a794279cab94
OtterBub <gamepsk@hanmail.net>
2015-05-05 09:38:22 +0900

    Iron Router

---
+++ Microscope/lib/router.js
@@ -0,0 +1,15 @@
+Router.configure({
+	layoutTemplate: 'layout',
+	loadingTemplate: 'loading',
+	notFoundTemplate: 'notFound',
+	waitOn: function() { return Meteor.subscribe('postsFilter'); }
+});
+
+
+
+Router.route('/', {name: 'postsList'});
+Router.route('/posts/:title', {
+	name: 'postPage',
+	data: function() { 
+		return Posts.findOne({title:this.params.title}); }
+});
