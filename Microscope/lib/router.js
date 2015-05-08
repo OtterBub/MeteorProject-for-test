0d002a479df80717e86ae0066060674d857c9b49
OtterBub <gamepsk@hanmail.net>
2015-05-08 17:12:04 +0900

    route.setParam Fixed

--- Microscope/lib/router.js
+++ Microscope/lib/router.js
@@ -9,7 +9,7 @@ Router.route('/', {name: 'postsList'});
 Router.route('/posts/:_id', {
 	name: 'postPage',
 	data: function() { 
-		return Posts.findOne({title:this.params._id}); }
+		return Posts.findOne(this.params._id); }
 });
 
 Router.onBeforeAction('dataNotFound', {only: 'postPage'});
